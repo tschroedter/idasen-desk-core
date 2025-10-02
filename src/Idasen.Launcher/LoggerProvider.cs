09d68ca222c1c93c1e028daef7f292a9b47be023
Thomas Schroedter <Thomas.Schroedter@gmx.net>
2025-10-02 19:10:47 +0930

    add log message in case of using default configuration

--- src/Idasen.Launcher/LoggerProvider.cs
+++ src/Idasen.Launcher/LoggerProvider.cs
@@ -59,14 +59,20 @@ public static class LoggerProvider
             if (!File.Exists(Path.Combine(baseDir, "appsettings.json")) &&
                 !File.Exists(Path.Combine(baseDir, $"appsettings.{environmentName}.json")))
             {
-                return CreateDefaultConfiguration ( ) ;
+                _logger = CreateDefaultConfiguration ( ) ;
             }
+            else
+            {
 
-            _logger = new LoggerConfiguration ( )
-                     .ReadFrom.Configuration ( configuration )
-                     .CreateLogger ( ) ;
+                _logger = new LoggerConfiguration ( )
+                         .ReadFrom.Configuration ( configuration )
+                         .CreateLogger ( ) ;
 
-            Log.Logger = _logger ;
+                Log.Logger = _logger ;
+
+                _logger.Information ( "Created logger from configuration file appsettings.{EnvironmentName}.json",
+                                      environmentName) ;
+            }
 
             return _logger ;
         }
@@ -214,35 +220,44 @@ public static class LoggerProvider
                AppContext.BaseDirectory ;
     }
 
-    private static Logger CreateDefaultConfiguration()
+    private static Logger CreateDefaultConfiguration ( )
     {
         // Default configuration based on provided JSON
-        _logger = new LoggerConfiguration()
-                 .MinimumLevel.Debug()
-                 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                 .MinimumLevel.Override("System", LogEventLevel.Information)
-                 .Enrich.FromLogContext()
-                 .Enrich.WithProperty("MachineName", Environment.MachineName)
-                 .Enrich.WithProperty("ProcessId", Environment.ProcessId)
-                 .Enrich.WithProperty("ThreadId", Environment.CurrentManagedThreadId)
-                 .WriteTo.Console(
-                                  outputTemplate: "{Timestamp:HH:mm:ss} [{Level}] (pid:{ProcessId} tid:{ThreadId} caller:{SourceContext}) {Message}{NewLine}{Exception}"
-                                 )
-                 .WriteTo.File(
-                               path: Path.Combine(
-                                                  Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
-                                                  "Idasen.SystemTray", "logs", "Idasen.SystemTray.log"
-                                                 ),
-                               outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] (pid:{ProcessId} tid:{ThreadId} caller:{SourceContext}) {Message}{NewLine}{Exception}",
-                               rollOnFileSizeLimit: true,
-                               fileSizeLimitBytes: 10485760,
-                               retainedFileCountLimit: 1,
-                               rollingInterval: RollingInterval.Infinite,
-                               hooks: new LoggingFileHooks()
-                              )
-                 .CreateLogger();
+        _logger = new LoggerConfiguration ( )
+                 .MinimumLevel.Information ( )
+                 .MinimumLevel.Override ( "Microsoft" ,
+                                          LogEventLevel.Information )
+                 .MinimumLevel.Override ( "System" ,
+                                          LogEventLevel.Information )
+                 .Enrich.FromLogContext ( )
+                 .Enrich.WithProperty ( "MachineName" ,
+                                        Environment.MachineName )
+                 .Enrich.WithProperty ( "ProcessId" ,
+                                        Environment.ProcessId )
+                 .Enrich.WithProperty ( "ThreadId" ,
+                                        Environment.CurrentManagedThreadId )
+                 .WriteTo
+                 .Console ( outputTemplate :
+                            "{Timestamp:HH:mm:ss} [{Level}] (pid:{ProcessId} tid:{ThreadId} caller:{SourceContext}) {Message}{NewLine}{Exception}" )
+                 .WriteTo
+                 .File ( path : Path.Combine ( Environment.GetFolderPath ( Environment.SpecialFolder
+                                                                                      .CommonApplicationData ) ,
+                                               "Idasen.SystemTray" ,
+                                               "logs" ,
+                                               "Idasen.SystemTray.log" ) ,
+                         outputTemplate :
+                         "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] (pid:{ProcessId} tid:{ThreadId} caller:{SourceContext}) {Message}{NewLine}{Exception}" ,
+                         rollOnFileSizeLimit : true ,
+                         fileSizeLimitBytes : 10485760 ,
+                         retainedFileCountLimit : 1 ,
+                         rollingInterval : RollingInterval.Infinite ,
+                         hooks : new LoggingFileHooks ( ) )
+                 .CreateLogger ( ) ;
 
-        Log.Logger = _logger;
-        return _logger;
+        Log.Logger = _logger ;
+
+        _logger.Information ( "Created default logger configuration" ) ;
+
+        return _logger ;
     }
 }
