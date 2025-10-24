37d82c7ff5a089cfb11ac04ab6ba21309a1772c9
Thomas Schroedter <Thomas.Schroedter@gmx.net>
2025-10-24 19:42:05 +1030

    fix minor ReSharper issues

--- src/Idasen.BluetoothLE.Linak/DeskConnectorFactories.cs
+++ src/Idasen.BluetoothLE.Linak/DeskConnectorFactories.cs
@@ -2,23 +2,15 @@ using Idasen.BluetoothLE.Linak.Interfaces;
 
 namespace Idasen.BluetoothLE.Linak
 {
-    public class DeskConnectorFactories : IDeskConnectorFactories
+    public class DeskConnectorFactories ( IDeskHeightAndSpeedFactory  heightAndSpeedFactory ,
+                                          IDeskCommandExecutorFactory commandExecutorFactory ,
+                                          IDeskMoverFactory           moverFactory ,
+                                          IDeskLockerFactory          lockerFactory )
+        : IDeskConnectorFactories
     {
-        public DeskConnectorFactories(
-            IDeskHeightAndSpeedFactory heightAndSpeedFactory,
-            IDeskCommandExecutorFactory commandExecutorFactory,
-            IDeskMoverFactory moverFactory,
-            IDeskLockerFactory lockerFactory)
-        {
-            HeightAndSpeedFactory = heightAndSpeedFactory;
-            CommandExecutorFactory = commandExecutorFactory;
-            MoverFactory = moverFactory;
-            LockerFactory = lockerFactory;
-        }
-
-        public IDeskHeightAndSpeedFactory HeightAndSpeedFactory { get; }
-        public IDeskCommandExecutorFactory CommandExecutorFactory { get; }
-        public IDeskMoverFactory MoverFactory { get; }
-        public IDeskLockerFactory LockerFactory { get; }
+        public IDeskHeightAndSpeedFactory  HeightAndSpeedFactory  { get; } = heightAndSpeedFactory ;
+        public IDeskCommandExecutorFactory CommandExecutorFactory { get; } = commandExecutorFactory ;
+        public IDeskMoverFactory           MoverFactory           { get; } = moverFactory ;
+        public IDeskLockerFactory          LockerFactory          { get; } = lockerFactory ;
     }
 }
